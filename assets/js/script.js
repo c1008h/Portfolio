ba9f49c9fd569dec25eebdc218160b266c847edb
Christina Hong <hongchristinaaa@gmail.com>
2022-06-04 23:02:04 -0700

    Working links, still need to format everything for it to look even better.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
 // Fork the Food buttons
 var forkFoodBtn = document.querySelector('#forkthefoodbtn')
-var backBtn = document.querySelector('#backbtn')
+var backBtn = document.getElementById('backbtn')
 // Code quiz buttons
-var codeQuizBtn = document.quearySelector('#codequizbtn')
+var codequizbtn = document.getElementById('codequizbtn')
 
+var backBtn = document.querySelector('#backbtn')
+
+backBtn.addEventListener('click', function(event) {
+    console.log(event.target)
+    window.location.href = 'portfolio.html'
+})
 
 forkFoodBtn.addEventListener('click', function(event) {
     console.log(event.target)
-    window.location.href = 'fork-the-food.html'
+    window.location.href = "fork-the-food.html"
 })
 
 
 
-codeQuizBtn.addEventListener('click', function(event) {
-    window.location.href = 'code-quiz.html'
+codequizbtn.addEventListener('click', function(event) {
+    window.location.href = "code-quiz.html"
 })
